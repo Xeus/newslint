8ce206588805529e0577b9dcc71877af3c4a5218
Ben Turner <exterior@benturner.com>
2013-11-20 02:29:40 -0500

    add missing main() functionality

--- joblint.py
+++ joblint.py
@@ -1,4 +1,6 @@
+import sys
 import linter_class
+import lintlog
 import rule.bro_culture as bro_culture
 import rule.bubble as bubble
 import rule.expectations as expectations
@@ -7,12 +9,12 @@ import rule.realism as realism
 import rule.sexism as sexism
 import rule.tech as tech
 
+
 def joblint(content):
     linter = linter_class.create()
     inject_rules_into_linter(linter)
-    # return linter.lint(content)
-    result = linter.lint(content)
-    print(result.fail_points, result.errors, result.warnings, result.notices)
+    return linter.lint(content)
+
 
 def inject_rules_into_linter(linter):
     bro_culture.define_rules(linter)
@@ -22,3 +24,11 @@ def inject_rules_into_linter(linter):
     realism.define_rules(linter)
     sexism.define_rules(linter)
     tech.define_rules(linter)
+
+
+def main():
+    result = joblint(sys.argv[1])
+    lintlog.report(result, {'verbose': False})
+
+if __name__ == "__main__":
+    main()
