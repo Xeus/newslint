3dd167f957357a437a5772ec215758caf7d974c3
Ben Turner <exterior@benturner.com>
2013-12-17 11:31:43 -0500

    fixed a test

--- tests.py
+++ tests.py
@@ -7,6 +7,11 @@ class TestLinter(unittest.TestCase):
     def setUp(self):
         self.result = newslint.newslint('brown shirt obamacare tom friedman hippies fucker')
 
+    def test_keys(self):
+        self.assertTrue('credibility' in self.result.fail_points)
+        self.assertTrue('nonpartisanship' in self.result.fail_points)
+        self.assertTrue('professionalism' in self.result.fail_points)
+
     def test_points(self):
         self.assertEqual(self.result.fail_points['credibility'], 2.0)
         self.assertEqual(self.result.fail_points['nonpartisanship'], 2.0)
